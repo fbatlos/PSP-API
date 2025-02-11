19365a4db229814fdd00842f237d2cd30e8ffb35
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2025-02-11 20:37:10 +0100

    Primer Commit

---
+++ PSP_API/Program.cs
@@ -0,0 +1,44 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+var scoresBD = new[]
+{
+    new Score("Fran",200)
+};
+
+
+app.MapGet("/players", () =>
+{
+    var scores =  scoresBD
+        .ToArray();
+    return scores;
+})
+.WithName("GetPlayers");
+
+app.MapPost("/player-points", (Score score) => {
+    
+    scoresBD.Append(score);
+    return score;
+}).WithName("PostPoints");
+
+app.Run();
+
+record Score(string name ,int points){
+
+}
+
+
